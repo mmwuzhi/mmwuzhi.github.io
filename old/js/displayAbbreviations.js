f08f091c2a948d5271441749f2dacf1687ec539a
mmwuzhi <mmwuzhi@gmail.com>
2021-05-08 23:31:25 +0900

    新版！！！

--- old/js/displayAbbreviations.js
+++
@@ -1,8 +0,0 @@
-function displayAbbreviations() {
-    let abbreviations = document.getElementsByName("abbr")
-    if(abbreviations.length < 1) return false 
-    let defs = new Array()
-    for(i=0;i<abbreviations.length;i++) {
-        let definition = abbreviations[i].getAttribute("title")
-    }
-}
